acefb1a10c2b5b00e960fc6cc08cdfd2f64b1128
Nitish Soman <nitish.soman13@gmail.com>
2021-05-31 09:18:15 -0400

    Add init

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+
+      <h1>Lets build Facebook</h1>
+    </div>
+  );
+}
