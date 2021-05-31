f6e8cd10a2433121ec0ccbb43e41925bc3641329
Nitish Soman <nitish.soman13@gmail.com>
2021-05-31 16:58:02 -0400

    Add right-side header components

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import {
+  BellIcon,
   BeltIcon,
   ChatIcon,
   ChevronDownIcon,
@@ -15,9 +16,11 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 
+import HeaderIcon from "./HeaderIcon";
+
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Left */}
       <div className="flex items-center">
         <Image
@@ -26,10 +29,10 @@ function Header() {
           height={40}
           layout="fixed"
         />
-        <div className="flex ml-2 item-center rounded-full bg-gray-100 p-2">
-          <SearchIcon className="h-6" />
+        <div className="flex ml-2 item-center rounded-full bg-gray-200 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent"
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none"
             type="text"
             placeholder="Search Facebook"
           />
@@ -37,8 +40,26 @@ function Header() {
       </div>
 
       {/* Center */}
-
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
       {/* Right */}
+
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile Pic */}
+
+        <p className="whitespace-nowrap font-semibold pr-3 ">Nitish Soman</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 }
